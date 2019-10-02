bd246c7d9e356d354131f1cdd230f4d23a257cdb
joey <joey.oosenbrug@gmail.comj>
2019-10-02 10:06:36 +0200

    menu screen with prices

--- javascript/webshop.js
+++ javascript/webshop.js
@@ -1,9 +1,8 @@
 let method = "";
 let category_id = "";
 
-window.onload = function () {
-    method = "categoriesWithProducts";
-    api()
+function prodClick() {
+    console.log(event.target.className);
 }
 
 function api() {
@@ -19,32 +18,63 @@ function api() {
 function categoryAdd(cat) {
     cat.forEach(cat => {
         let btncategory = document.createElement("button")
-        btncategory.className = "menu__button";
+        btncategory.className = "product__button";
 
         let category = document.createElement("article")
-        category.className = "menu__items";
+        category.className = "product__items";
 
         let catcontainer = document.createElement("div")
-        catcontainer.className = "menu__container";
+        catcontainer.className = "product__container";
+
+        let cattitles = document.createElement("div")
+        cattitles.className = "product__title";
+
+        let catpricename = document.createElement("h3")
+        catpricename.className = "product__pricename";
+
         let catname = document.createElement("h3")
-        catname.className = "menu__name";
+        catname.className = "product__nametitle";
 
 
         catname.innerHTML = cat.name;
+        catpricename.innerHTML = "Price";
 
 
-
-        catcontainer.appendChild(catname);
+        cattitles.appendChild(catname);
+        cattitles.appendChild(catpricename);
+        category.appendChild(cattitles);
         category.appendChild(catcontainer);
 
+
             cat.products.forEach(catProd => {
+                let prodcontainer = document.createElement("div")
+                prodcontainer.className = "product__contain";
+
                 let productname = document.createElement("p");
-                productname.className = "product__name" + cat.name;
+                productname.className = "product__name " + catProd.name;
                 productname.innerHTML = catProd.name;
                 catcontainer.appendChild(productname);
+
+                let productPrice = document.createElement("p");
+                productPrice.className = "product__price";
+                productPrice.innerHTML = catProd.price + "$";
+                catcontainer.appendChild(productPrice);
+
+                prodcontainer.appendChild(productname);
+                prodcontainer.appendChild(productPrice);
+                category.appendChild(prodcontainer);
+
+                productname.addEventListener("click", function() {
+                    prodClick();
+                })
             })
 
         document.getElementById("products").appendChild(category);
     })
 
+}
+
+window.onload = function () {
+    method = "categoriesWithProducts";
+    api()
 }
